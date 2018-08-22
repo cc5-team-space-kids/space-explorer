eadd9fbe864059b34185b4effb244f9b18f744fe
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-08-22 09:36:19 +0900

    Add google map with search box

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GoogleMap></GoogleMap>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import GoogleMap from './components/GoogleMap.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    GoogleMap,
   }
 }
 </script>
