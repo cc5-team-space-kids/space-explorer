5d6a3924877299eac124bf06aa395034aacd174e
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-09-20 15:30:34 +0900

    added card layout

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -5,26 +5,64 @@
         <h1>Space Explorer</h1>
         <p>{{this.$store.state.intro}}</p>
         </div>
-      <h2>Satellite Information</h2>
-      <div>
-        Current Satellites is: {{this.$store.state.center.name}}<br/>
-        Latitude: {{this.$store.state.center.lat}} Longitude: {{this.$store.state.center.lng}}<br/>
-      </div>
-      <h2>Sun Information</h2>
-      dawn: {{time(this.$store.state.sunInfo[0].dawn)}}<br/>
-      sunset: {{time(this.$store.state.sunInfo[1].sunset)}}<br/>
-      noon: {{time(this.$store.state.sunInfo[2].noon)}}<br/>
-      sunrise: {{time(this.$store.state.sunInfo[3].sunrise)}}<br/>
-      dusk: {{time(this.$store.state.sunInfo[4].dusk)}}
-      <h2>Moon Information</h2>
-      age: {{this.$store.state.moonInfo[0].age}}<br/>
-      illumination: {{this.$store.state.moonInfo[0].illumination}}<br/>
-      stage: {{this.$store.state.moonInfo[0].stage}}<br/>
-      DFCOE: {{this.$store.state.moonInfo[0].DFCOE}}<br/>
-      DFS: {{this.$store.state.moonInfo[0].DFS}}<br/>
-      FM- UT: {{time(this.$store.state.moonInfo[0].FM.UT)}} DT: {{time(this.$store.state.moonInfo[0].FM.DT)}}<br/>
-      NNM- UT:{{time(this.$store.state.moonInfo[0].NNM.UT)}}  DT:{{time(this.$store.state.moonInfo[0].NNM.DT)}}
-      <div class="features">
+     
+      <v-card>
+      <v-container
+        fluid
+        grid-list-lg
+      >
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-card color="blue-grey darken-2" class="white--text">
+               <v-card-title primary-title>
+              <div>
+                <h2>Satellite Information</h2>
+                  <div>
+                    Current Satellites is: {{this.$store.state.center.name}}<br/>
+                    Latitude: {{this.$store.state.center.lat}} Longitude: {{this.$store.state.center.lng}}<br/>
+                  </div>
+              </div>
+            </v-card-title>
+             
+            </v-card>
+          </v-flex>
+          <v-flex xs12>
+            <v-card color="blue-grey darken-2" class="white--text">
+               <v-card-title primary-title>
+              <div>
+                  <h2>Sun Information</h2>
+                  dawn: {{time(this.$store.state.sunInfo[0].dawn)}}<br/>
+                  sunset: {{time(this.$store.state.sunInfo[1].sunset)}}<br/>
+                  noon: {{time(this.$store.state.sunInfo[2].noon)}}<br/>
+                  sunrise: {{time(this.$store.state.sunInfo[3].sunrise)}}<br/>
+                  dusk: {{time(this.$store.state.sunInfo[4].dusk)}}
+              </div>
+            </v-card-title>
+             
+            </v-card>
+          </v-flex>
+          <v-flex xs12>
+            <v-card color="blue-grey darken-2" class="white--text">
+               <v-card-title primary-title>
+              <div>
+                <h2>Moon Information</h2>
+                age: {{this.$store.state.moonInfo[0].age}}<br/>
+                illumination: {{this.$store.state.moonInfo[0].illumination}}<br/>
+                stage: {{this.$store.state.moonInfo[0].stage}}<br/>
+                DFCOE: {{this.$store.state.moonInfo[0].DFCOE}}<br/>
+                DFS: {{this.$store.state.moonInfo[0].DFS}}<br/>
+                FM- UT: {{time(this.$store.state.moonInfo[0].FM.UT)}} DT: {{time(this.$store.state.moonInfo[0].FM.DT)}}<br/>
+                NNM- UT:{{time(this.$store.state.moonInfo[0].NNM.UT)}}  DT:{{time(this.$store.state.moonInfo[0].NNM.DT)}}
+              </div>
+            </v-card-title>
+             
+            </v-card>
+          </v-flex>
+
+        </v-layout>
+      </v-container>
+    </v-card>
+    <div class="features">
         <h2>Features</h2>
         <div class="text" v-for="text in this.$store.state.texts" v-bind:key="text.id">
           <div>{{text.feature}}: {{text.detail}}</div>
@@ -32,6 +70,7 @@
       </div>
     </div>
   </div>
+  
 </template>
 
 <script>
@@ -47,7 +86,9 @@ export default {
 </script>
 
 <style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Nunito');
   .description {
+    font-family: 'Nunito', sans-serif;
     padding: 35px 5px;
     background-color: white;
     text-align: left;
